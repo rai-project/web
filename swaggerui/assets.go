a54af60e34b6b5fe6676c74fb2c0721b27ef7b55
Abdul Dakkak <abduld@wolfram.com>
2017-06-17 22:52:59 +0100

    fix paths

--- swaggerui/assets.go
+++ swaggerui/assets.go
@@ -2,14 +2,12 @@ package swaggerui
 
 import (
 	"net/http"
-	"path/filepath"
 
 	assetfs "github.com/elazarl/go-bindata-assetfs"
-	"github.com/k0kubun/pp"
 	"github.com/labstack/echo"
 )
 
-func getAssetFS() *assetfs.AssetFS {
+func AssetFS() *assetfs.AssetFS {
 	return &assetfs.AssetFS{
 		Asset:     Asset,
 		AssetDir:  AssetDir,
@@ -19,34 +17,5 @@ func getAssetFS() *assetfs.AssetFS {
 }
 
 func Handle() echo.HandlerFunc {
-	return echo.WrapHandler(http.FileServer(getAssetFS()))
-}
-
-func Routes(prefix string, e *echo.Echo) error {
-	mimeTypes := map[string]string{
-		".js":   echo.MIMEApplicationJavaScript,
-		".json": echo.MIMEApplicationJSONCharsetUTF8,
-		".html": echo.MIMETextHTMLCharsetUTF8,
-		".txt":  echo.MIMETextPlainCharsetUTF8,
-		".png":  "image/png",
-		".jpg":  "image/jpeg",
-		".jpeg": "image/jpeg",
-		".css":  "text/css",
-	}
-	for _, k := range AssetNames() {
-		bts, err := Asset(k)
-		if err != nil {
-			continue
-		}
-		k = prefix + "/" + k
-		mimeType, ok := mimeTypes[filepath.Ext(k)]
-		if !ok {
-			mimeType = "application/octet-stream"
-		}
-		pp.Println(k, "               ", mimeType)
-		e.GET(k, func(ctx echo.Context) error {
-			return ctx.Blob(http.StatusOK, mimeType, bts)
-		})
-	}
-	return nil
+	return echo.WrapHandler(http.FileServer(AssetFS()))
 }
