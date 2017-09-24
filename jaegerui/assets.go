856ac84ac02e8c663cf8f2c54fd6a898b02c4306
Abdul Dakkak <abduld@wolfram.com>
2017-09-24 01:43:20 -0500

    update

---
+++ jaegerui/assets.go
@@ -0,0 +1,21 @@
+package jaegerui
+
+import (
+	"net/http"
+
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/labstack/echo"
+)
+
+func AssetFS() *assetfs.AssetFS {
+	return &assetfs.AssetFS{
+		Asset:     Asset,
+		AssetDir:  AssetDir,
+		AssetInfo: AssetInfo,
+		Prefix:    "",
+	}
+}
+
+func Handle() echo.HandlerFunc {
+	return echo.WrapHandler(http.FileServer(AssetFS()))
+}
